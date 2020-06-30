ab51ae4f340d034daaf705a99998844b003c00b0
xboxtracer <xboxtracer@yandex.ru>
2020-06-30 10:34:41 +0300

    Закончил задачи 1 и 2 домашки #4

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,15 +1,54 @@
 'use strict'
 
-function getSolutions(a, b, c) {
-    let d = Math.pow(b, 2) - (4 * a * c);
+function showSolutionsMessage(a, b, c, d, x1, x2) {
+    console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+    console.log(`Значение дискриминанта: ${d}`);
     if (d < 0) {
-        return `{ D: ${d}, roots: [  ] }`
+        console.log('Уравнение не имеет вещественных корней')
     } else if (d === 0) {
-        let x1 = (- b) / (2 * a)
-        return `{ D: ${d}, roots: [ ${x1} ] }`
+        console.log(`Уравнение имеет один корень X₁ = ${x1}`)
     } else if (d > 0) {
-        // let x1 = (-b + √D) / 2a
-        // let x2 = (-b - √D) / 2a
-        return `{ D: ${d}, roots: [ ${x1} , ${x2} ] }`
+        console.log(`Уравнение имеет два корня. X₁ = ${x1}, X₂ = ${x2}`)
     }
 }
+
+function getSolutions(a, b, c) {
+    let d = Math.pow(b, 2) - (4 * a * c);
+    let result = {D: d, roots: []};
+    if (d === 0) {
+        let x1 = (- b) / (2 * a)
+        result.roots[0] = x1
+    } else if (d > 0) {
+        let x1 = (- b + Math.sqrt(d)) / (2 * a)
+        let x2 = (- b - Math.sqrt(d)) / (2 * a)
+        result.roots[0] = x1
+        result.roots[1] = x2
+    }
+    showSolutionsMessage(a, b, c, d, result.roots[0], result.roots[1]);
+    return result
+}
+
+function getAverageMark(marks) {    
+    let avg = 0;
+    let i = 0;
+    for (let key in marks) {
+        avg += marks[key];
+        i++
+    };
+    console.log(avg)
+    if (i === 0) {
+        i++
+    }
+    avg = avg / i;
+    return avg
+}
+function getAverageScore(data) {
+    let avgMarks = {}
+    for (let key in data) {
+        avgMarks[key] = getAverageMark(data[key]);
+    };
+    avgMarks.average = getAverageMark(avgMarks)
+    return avgMarks
+}
+
+console.log(getAverageMark([]))
