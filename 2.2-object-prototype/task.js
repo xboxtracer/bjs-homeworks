a896214f94a87c0c1b1abb2be8cea2fc90b969d6
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-07-01 12:07:52 +0300

    Домашка №5 завершена

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,21 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    return this.valueOf().toLowerCase().replace(/ /g, '').split('').reverse().join('') === this.valueOf().toLowerCase().replace(/ /g, '')
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let averageMark = 0;
+    if (typeof marks[0] !== 'number') {
+        return averageMark
+    }
+    marks.forEach(element => {
+        averageMark += element
+    });
+    
+    return Math.round(averageMark / marks.length)
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let today = new Date();
+    let bd = new Date(birthday)
+    return ((today - bd) / 1000 / 60 / 60 / 24 / 30 / 12) > 18 ? true : false
 }
