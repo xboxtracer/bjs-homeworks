b0b004e7592368946b2f4edb37225b8a9b7eda09
Ilya Kurbatov <xboxtracer@yandex.ru>
2020-07-03 22:04:03 +0300

    Домашка 8, задача 1 решена на 50%

---
+++ 3.2-array-proto-hocs/task.js
@@ -0,0 +1,30 @@
+'use strict'
+
+function sleep(milliseconds) {
+  let e = new Date().getTime() + milliseconds;
+  while (new Date().getTime() <= e) {
+      console.log(1)
+  }
+}
+
+function sum(...args) {
+    sleep(200);
+    console.log(...args)
+    return args.reduce((sum, arg) => {
+      return sum += +arg;
+    }, 0);
+}
+
+function compareArrays(arr1, arr2) {
+    function isLog(elem, index, array) {
+        if(array.length !== arr2.length) {
+            return false
+        }
+        return elem == arr2[index]
+    }
+    return arr1.every(isLog)
+}
+
+function memorize(fn, limit) {
+    return fn
+}
